e75fd8319a361293262b3ac5bc59c58ad3d69caa
riley <alkalinarwhal@gmail.com>
2024-03-11 19:49:46 -0700

    fu

--- js/base.js
+++ js/base.js
@@ -33,7 +33,7 @@ newListButton.addEventListener("click", function ()
 		if (event.keyCode === 13)
 		{
 			/** extract value from input box, sanitize */
-			let value = input.value.replace(/[^a-zA-Z0-9 ]/g, '');;
+			let value = input.value.replace(/[^a-zA-Z0-9 ]/g, '');
 
 			/** remove the text box */
 			this.remove();
